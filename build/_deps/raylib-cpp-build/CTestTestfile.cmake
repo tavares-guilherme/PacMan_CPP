f974be9bf96cd66237babb204192cfebce947296
Breno Alves <brenower@outlook.com>
2020-11-16 13:53:33 -0300

    Add .gitignore

--- build/_deps/raylib-cpp-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guilherme/Área de Trabalho/PacMan/build/_deps/raylib-cpp-src
-# Build directory: /home/guilherme/Área de Trabalho/PacMan/build/_deps/raylib-cpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
